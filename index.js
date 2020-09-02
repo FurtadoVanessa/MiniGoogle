16c43a864306fb8324049a55ed1d0abeb1d4def4
Cristiano Fagundes <fagundescristianof@gmail.com>
2020-09-02 19:04:42 -0300

    calculo do cosseno pronto

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var natural = require("natural"); //radicalizar as palavras
 portuguesSw = sw.ptbr;
 
 var url = [
-  "http://localhost/index.html",
-  "http://localhost/Historia.html",
-  "http://localhost/Cassinos.html",
+  "https://furtadovanessa.github.io/ProgramacaoWebI/Interdisciplinar/index.html",
+  "https://furtadovanessa.github.io/ProgramacaoWebI/Interdisciplinar/rachel.html",
+  "https://furtadovanessa.github.io/ProgramacaoWebI/Interdisciplinar/ross.html",
+  "https://furtadovanessa.github.io/ProgramacaoWebI/Interdisciplinar/monica.html",
+  "https://furtadovanessa.github.io/ProgramacaoWebI/Interdisciplinar/primeira.html",
   // "https://pocos.000webhostapp.com/PontosTuristicos.html",
   // "https://pocos.000webhostapp.com/Industrias.html",
   // "https://pocos.000webhostapp.com/Geografia.html",
@@ -19,13 +21,13 @@ var url = [
 ];
 var storedText;
 
-var matriz = {};
+var matriz = [];
 
-for (var i = 0; i < url.length; i++) {
+for (var i = 0; i <= url.length; i++) {
   matriz[i] = [];
 }
 
-var chave = "pocos";
+var chave = "rachel";
 
 var hash = new Map();
 
@@ -44,47 +46,36 @@ async function getTextos() {
   textosTratados.forEach((value) => {
     const conjuntoPalavras = value[0];
     const link = value[1];
-    console.log(
-      "estamos no arquivo ",
-      link,
-      "que tem ",
-      conjuntoPalavras.length
-    );
-    conjuntoPalavras.forEach((palavra) => {
-      var palavra1 = natural.PorterStemmerPt.stem(palavra);
-      if (hash.get(palavra1) != null) {
-        var col = hash.get(palavra1);
-        matriz[link][col] == null
-          ? (matriz[link][col] = 1)
-          : (matriz[link][col] += 1);
-        // console.log(
-        //   "ja foi criado a palavra ",
-        //   palavra1,
-        //   "na coluna ",
-        //   col,
-        //   "que agora vale ",
-        //   matriz[link][col]
-        // );
-      } else {
-        hash.set(palavra1, coluna);
-        matriz[link][coluna] = 1;
-        // console.log(
-        //   "criando a palavra ",
-        //   palavra1,
-        //   "na coluna ",
-        //   coluna,
-        //   "que agora vale ",
-        //   matriz[link][coluna]
-        // );
-        coluna++;
-      }
-    });
+
+  popularMatriz(conjuntoPalavras, link);
+
   });
-  mostrarMatriz();
   normalizarMatriz();
   calcularMatriz();
-  mostrarMatriz();
+  //mostrarMatriz();
+
   var chaveProcessada = done(chave);
+  popularMatriz(chaveProcessada, url.length);
+  similaridadeDoCosseno();
+
+}
+
+
+function popularMatriz(conjuntoPalavras, link){
+
+  conjuntoPalavras.forEach((palavra) => {
+    var palavra1 = natural.PorterStemmerPt.stem(palavra);
+    if (hash.get(palavra1) != null) {
+      var col = hash.get(palavra1);
+      matriz[link][col] == null
+        ? (matriz[link][col] = 1)
+        : (matriz[link][col] += 1);
+    } else {
+      hash.set(palavra1, coluna);
+      matriz[link][coluna] = 1;
+      coluna++;
+    }
+  });
 }
 
 function normalizarMatriz() {
@@ -93,9 +84,12 @@ function normalizarMatriz() {
     if (matriz[linha].length > maior) {
       maior = matriz[linha].length;
     }
+    for(var i=0; i<matriz[linha].length; i++){
+      if(!matriz[linha][i]){
+        matriz[linha][i] = 0;
+      }
+    }
   }
-  console.log("o maior arquivo tem ", maior);
-
   for (linha in matriz) {
     var tamanho = matriz[linha].length;
     while (tamanho < maior) {
@@ -108,11 +102,12 @@ function normalizarMatriz() {
 function calcularMatriz() {
   var j = 0;
   for (linha in matriz) {
-    console.log("fazendo linha ", linha, "com length ", matriz[linha].length);
     matriz[linha].forEach((coluna, index) => {
       var tf = 0;
-      if (matriz[j][index]) {
+      if (matriz[j][index] > 0) {
         tf = 1 + Math.log(matriz[j][index]);
+      }else {
+        tf = 0;
       }
       var idf = calculaIdf(index);
       matriz[j][index] = tf * idf;
@@ -127,12 +122,12 @@ function calculaIdf(index) {
   var quantidade = url.length;
 
   for (linha in matriz) {
-    if (matriz[linha][index] != null) {
+    if (matriz[linha][index] > 0) {
       ocorrencias += 1;
     }
   }
 
-  return quantidade / ocorrencias;
+  return Math.log(quantidade / ocorrencias);
 }
 
 getTextos();
@@ -149,13 +144,61 @@ function done(item) {
 
 function mostrarMatriz() {
   for (var i = 0; i < url.length; i++) {
-    console.log("linha ", i);
-    console.log(matriz[i][0]);
+    for(var j = 0; j<20; j++){
+      console.log("linha ", i, "coluna ", j);
+      console.log(matriz[i][j]);
+    }
   }
 }
+
+
+function similaridadeDoCosseno(){
+  var vetorL2 = [];
+  var vetorProdutoEscalar = [];
+
+  var vetorCosseno = [];
+
+  for(linha in matriz){
+    vetorL2[linha] = calculaL2(linha);
+    vetorProdutoEscalar[linha] = produtoEscalar(linha);
+    //console.log("para o arquivo ", linha, " o l2 vale ", vetorL2[linha], " e o escalar vale ", vetorProdutoEscalar[linha]);
+  }
+
+  var normaDaChave = vetorL2[vetorL2.length-1];
+
+  for(var i=0; i<vetorL2.length-1; i++){
+    vetorCosseno[i] = vetorProdutoEscalar[i]/(normaDaChave*vetorL2[i]);
+    console.log("para o arquivo ", i, " o cosseno eh ", vetorCosseno[i]);
+  }
+
+
+
+
+}
+
 //inserir a chave de busca na tabela (fazer todo o processo de normalização), e colocar o valor 1 na chave que tiver
 
 //norma L2 - eleva todos os valores da linha ao quadrado e soma, depois tira a raiz de tudo, calcular tambem da chave de busca
+function calculaL2(linha){
+  var soma = 0;
+  for(var i=0; i<matriz[linha].length; i++){
+    soma = soma + matriz[linha][i]*matriz[linha][i];
+  }
+  return Math.sqrt(soma);
+}
+
+
+function produtoEscalar(linha){
+    var linhaDaChave = url.length;
+    var soma = 0;
+    for(var i=0; i<matriz[linha].length; i++){
+      soma = soma + matriz[linha][i]*matriz[linhaDaChave][i];
+    }
+    return soma;
+}
+
+
+
 //produto escalar - multiplicar cada valor da linha do arquivo pelo valor da linha da chave de busca
 //cosseno = produto escalar / (norma da chave * norma do arquivo)
 
